d60b710d0ddd17916b2ec4ab3c961a83b0fc5d60
Richetti123 <148177322+Richetti123@users.noreply.github.com>
2023-12-14 22:08:39 -0500

    Update grupo-tagall.js

--- plugins/grupo-tagall.js
+++ plugins/grupo-tagall.js
@@ -6,10 +6,10 @@ const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, com
   }
   const pesan = args.join` `;
 let oi = `*🌟𝙈𝙚𝙧𝙧𝙮 𝙘𝙝𝙧𝙞𝙨𝙩𝙢𝙖𝙨🌟* ${pesan}`
-let teks = `*😸𝘎𝘖𝘎𝘖! 𝘈𝘤𝘵𝘪𝘷𝘦𝘯𝘴𝘦𝘦.. 𝘗𝘜𝘛𝘟𝘚!😼*\n\n${oi}\n*🩵@𝖈𝖊𝖔𝖌𝖊𝖗𝖎𝖕𝖎𝖚𝖒𝖉𝖟𝖓*\n`
+let teks = `*Activense Mancos 😎*\n\n${oi}\n*MENCIONES:*\n`
 for (let mem of participants) {
-teks += `☃️❄️ @${mem.id.split('@')[0]}\n`}
-teks += `MichiBot`
+teks += `✨👊🏼 @${mem.id.split('@')[0]}\n`}
+teks += `KaizenBot`
   conn.sendMessage(m.chat, {text: teks, mentions: participants.map((a) => a.id)} );
 };
 handler.help = ['tagall <mesaje>', 'invocar <mesaje>'];
